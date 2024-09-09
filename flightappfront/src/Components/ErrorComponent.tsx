9dc08457ce71928f46b88b3670941dcfe077fb2e
jorgencora <jorge.cardenas@encora.com>
2024-09-09 16:05:43 -0700

    [Back] Update files removed from gitignore

--- flightappfront/src/Components/ErrorComponent.tsx
+++ flightappfront/src/Components/ErrorComponent.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import { ErrorResponse, isRouteErrorResponse, Link, useNavigate, useRouteError } from "react-router-dom";
+import { ErrorResponse, Link, useRouteError } from "react-router-dom";
 
 export default function ErrorComponent(){
 
